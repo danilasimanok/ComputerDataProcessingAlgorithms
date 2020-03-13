94e4e98f25bb7cc3f322f68392d9d697ff0ee6de
DanilaSimanok <dsimanok@bk.ru>
2020-03-13 17:48:00 +0300

    Это надо переделать.

---
+++ hw2/Tasks/Utils/Library.fs
@@ -0,0 +1,35 @@
+﻿namespace Utils
+
+module Matrix =
+    
+    type Matrix<'a> = 
+        | Rows of 'a list list
+        | Columns of 'a list list
+
+    type Semiring<'a> =
+        {
+        IdentityElement: 'a;
+        Add: ('a -> 'a -> 'a);
+        Multiply: ('a -> 'a -> 'a)
+        }
+
+    (* Не забудь реализовать. *)
+    let columnsToRows columns = [[]]
+
+    (* Не забудь реализовать. *)
+    let rowsToColumns rows = [[]]
+
+    let multiplyRC sr row column = List.fold sr.Add (List.map sr.Multiply (List.zip row column)) sr.IdentityElement
+
+    let multiplyWithColumns sr columns row = List.map (multiplyRC sr row) columns
+
+    let mulpiply sr a b =
+        let m1 =
+            match a with
+            | Rows(rows) -> rows
+            | Columns(columns) -> columnsToRows columns
+        let m2 =
+            match b with
+            | Rows(rows) -> rowsToColumns rows
+            | Columns(columns) -> columns
+        List.map (multiplyWithColumns sr m2) m1
